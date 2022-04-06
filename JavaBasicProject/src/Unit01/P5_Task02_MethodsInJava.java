20a15e8d29bdbdafd9e35faa17d5bf584c68734b
praveenkumar <praveenkumar@localhost>
2022-04-06 14:18:45 +0530

    commit 5 (03) | completed P5 Task02

---
+++ JavaBasicProject/src/Unit01/P5_Task02_MethodsInJava.java
@@ -0,0 +1,28 @@
+package Unit01;
+
+public class P5_Task02_MethodsInJava {
+
+	public static void main(String[] args) {
+		classInP5 obj = new classInP5();
+		obj.display1(); // we can also call static methods by objects
+		System.out.println(obj.a);
+		
+	}
+
+}
+
+class classInP5{
+	static int a = 10;
+	int b = 30;
+	
+	static void display1() {
+		int a = 5;
+		int b = 6;
+		System.out.println(a + " | " + b);
+	}
+	
+	int display2() {
+		System.out.println(a);
+		return a;
+	}
+}
